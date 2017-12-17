220bed391177d303efb3712e4d3145e66ffba962
Paulo Alves <pauloalvsjr@gmail.com>
2017-12-17 03:24:25 -0200

    Multifile Upload

--- dontfile.go
+++ dontfile.go
@@ -1,9 +1,10 @@
 package main
 
 import (
-	"encoding/json"
+	//"encoding/json"
 	"fmt"
 	"html/template"
+	"io"
 	"io/ioutil"
 	"net/http"
 	"os"
@@ -15,7 +16,7 @@ import (
 
 type Room struct {
 	Directory string
-	Files     []string
+	Files     []os.FileInfo
 }
 
 func main() {
@@ -46,41 +47,38 @@ func fileUpload(w http.ResponseWriter, r *http.Request) {
 	dir := r.URL.Path[1:]
 
 	if r.Method == http.MethodPost {
-		upload, header, err := r.FormFile("file")
-		if err != nil {
-			panic(err)
-		}
-		defer upload.Close()
+		_ = r.ParseMultipartForm(100000)
+		m := r.MultipartForm
 
-		fin := header.Filename
+		files := m.File["file"]
 
-		fileBytes, err := ioutil.ReadAll(upload)
-		if err != nil {
-			panic(err)
-		}
+		for i := range files {
+			file, _ := files[i].Open()
+			defer file.Close()
 
-		cmd := exec.Command("mkdir", "storage/"+dir)
-		cmd.Run()
+			cmd := exec.Command("mkdir", "storage/"+dir)
+			cmd.Run()
 
-		err = ioutil.WriteFile("storage/"+dir+"/"+fin, fileBytes, 0644)
-		if err != nil {
-			panic(err)
+			destinationFile, _ := os.Create("storage/" + dir + "/" + files[i].Filename)
+			defer destinationFile.Close()
+
+			io.Copy(destinationFile, file)
 		}
 	}
 
 	files, _ := ioutil.ReadDir("storage/" + dir)
 
-	var ffiles []string
+	room := Room{dir, files}
 
-	for _, f := range files {
-		ffiles = append(ffiles, f.Name())
-	}
+	t, _ := template.ParseFiles("views/upload.html")
+	t.Execute(w, room)
 
-	room := Room{dir, ffiles}
+	/*
+		json, _ := json.Marshal(room)
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(json)
+	*/
 
-	json, _ := json.Marshal(room)
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(json)
 }
 
 func fileDownload(w http.ResponseWriter, r *http.Request) {
@@ -89,15 +87,13 @@ func fileDownload(w http.ResponseWriter, r *http.Request) {
 }
 
 func fileDelete(w http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodPost {
-		dir := "storage/" + r.URL.Path[1:]
-		dir = strings.TrimSuffix(dir, "/delete")
+	dir := "storage/" + r.URL.Path[1:]
+	dir = strings.TrimSuffix(dir, "/delete")
 
-		cmd := exec.Command("rm", "-rf", dir)
-		cmd.Run()
+	cmd := exec.Command("rm", "-rf", dir)
+	cmd.Run()
 
-		dirs := strings.Split(dir, "/")
+	dirs := strings.Split(dir, "/")
 
-		http.Redirect(w, r, "/"+dirs[1], 301)
-	}
+	http.Redirect(w, r, "/"+dirs[1], 301)
 }
