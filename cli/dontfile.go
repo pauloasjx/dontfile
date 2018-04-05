c5b9b3e00e900689aafb8363585305d519244195
Paulo Alves <juninhpaulo@hotmail.com>
2018-04-05 16:48:33 -0300

    CLI ls cd rm pwd get

--- cli/dontfile.go
+++ cli/dontfile.go
@@ -2,25 +2,63 @@ package main
 
 import (
 	"fmt"
-	//"strings"
+	"strings"
 	"net/http"
 	"io/ioutil"
 	"io"
-	"net/url"
+	//"net/url"
 	"os"
+	"time"
+	"encoding/json"
 )
 
-var base string = "http://localhost:3001/"
-var dir string = "test"
-
-func main() {
-	
-	upload("test.txt")
+type FileInfo struct {
+    Name    string
+	Size    int64
+	Dir		bool
+    ModTime time.Time
 }
 
-func download(file string) {
-	fileUrl := base + dir + file
+type Room struct {
+	Directory string
+	Files     []FileInfo
+}
 
+func (r *Room) files() []string {
+	files := []string{}
+
+	for _, file := range r.Files {
+		files = append(files, file.Name)
+	}
+
+	return files
+}
+
+var base string = "http://localhost:3002"
+var dir string = ""
+
+func main() {
+	var opt, arg string
+
+	for opt != "exit" {	
+		fmt.Printf("<Dontfile (%s)> ", base + dir)
+		switch fmt.Scanln(&opt, &arg); opt {
+			case "ls": 
+				ls()
+			case "pwd":
+				pwd()
+			case "cd":
+				cd(arg)
+			case "rm":
+				rm(arg)
+			case "get":
+				get(arg)
+		}
+	}
+}
+
+func get(file string) {
+	fileUrl := base + dir + "/" + file
 	fileOut, _ := os.Create(file)
 	defer fileOut.Close()
 
@@ -30,6 +68,7 @@ func download(file string) {
 	io.Copy(fileOut, res.Body)
 }
 
+/*
 func upload(file string) {
 	fileUpload, _ := os.Open(file)
 	defer fileUpload.Close()
@@ -40,20 +79,41 @@ func upload(file string) {
 
 	fmt.Println("Ok")
 }
+*/
 
 func ls() {
-	res, _ := http.Get(base + dir)
+	res, _ := http.Get(base + "/" + dir)
 	defer res.Body.Close()
 
 	body, _ := ioutil.ReadAll(res.Body)
+	var room Room
 
-	fmt.Println(string(body))
+	json.Unmarshal(body, &room)
+	fmt.Println(room.files())
 }
 
 func cd(subdir string) {
-	dir += "/" + subdir
+	switch subdir {
+		case "/":
+			dir = "/"
+		case ".":
+		case "..":
+			dir = strings.TrimSuffix(dir, "/" + strings.Split(dir, "/")[strings.Count(dir, "/")])
+		default:
+			dir += "/" + subdir
+	}
+}
+
+func rm(file string) {
+	fileUrl := base + dir + "/" + file
+
+	fmt.Println(fileUrl)
+
+	req, _ := http.NewRequest("DELETE", fileUrl, nil)
+	res, _ := http.DefaultClient.Do(req)
+	defer res.Body.Close()
 }
 
 func pwd() {
-	fmt.Println(dir)
+	fmt.Println(base + "/"  + dir)
 }
